31efede0aa8f3bc0ddca6c66f0e2ee8d30140b9e
hari2095 <hari2095@gmail.com>
2015-06-08 19:28:10 +0530

    changed index.js to opentok code

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-var express = require('express');
-var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+var OpenTok = require('opentok'),
+    opentok = new OpenTok(45250532,'54afc8f4b42cbec283a979313eea14ea398fb221');
+
+// Create a session that will attempt to transmit streams directly between
+// clients. If clients cannot connect, the session uses the OpenTok TURN server:
+opentok.createSession(function(err, session) {
+  if (err) return console.log(err);
+
+  // save the sessionId
+  //db.save('session', session.sessionId, done);
+  // Genrate a Token from a session object (returned from createSession)
+  token = session.generateToken();
+  console.log("Token created");
+  console.log(token);
+
+  var publisher;
+var targetElement = 'publisherContainer';
+
+publisher = opentok.initPublisher(targetElement, null, function(error) {
+  if (error) {
+    // The client cannot publish.
+    // You may want to notify the user.
+  } else {
+    console.log('Publisher initialized.');
+  }
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+
+
 });
+
